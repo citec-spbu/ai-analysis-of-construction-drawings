87bf9611440a5381f1f44eb9cc5a2be1395ebc2f
nat5natalia <nat5natalia@yandex.ru>
2026-04-26 21:44:12 +0300

    add dynamic rag and cache fix 2

--- drawing_agent/rag/embeddings.py
+++ drawing_agent/rag/embeddings.py
@@ -2,7 +2,7 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 
 class EmbeddingGenerator:
-    def __init__(self, model_name='paraphrase-multilingual-MiniLM-L12-v2'):
+    def __init__(self, model_name='all-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
 
     def generate(self, text: str) -> np.ndarray:
